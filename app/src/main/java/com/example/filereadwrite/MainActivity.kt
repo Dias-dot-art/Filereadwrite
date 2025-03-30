860bb072951b272595a848bf8141e7ca230e0a7c
Dias-dot-art <129710179+Dias-dot-art@users.noreply.github.com>
2025-03-30 21:28:19 +0500

    Implemented write file functionality

--- app/src/main/java/com/example/filereadwrite/MainActivity.kt
+++ app/src/main/java/com/example/filereadwrite/MainActivity.kt
@@ -1,20 +1,120 @@
 package com.example.filereadwrite
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Environment
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.ByteArrayOutputStream
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvData: TextView
+
+    companion object {
+        const val REQUEST_CODE_WRITE_PERM = 401
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvData = findViewById(R.id.tvData)
+
+        val btnWriteFile: Button = findViewById(R.id.btnWriteFile)
+        btnWriteFile.setOnClickListener {
+            writeFile("Hello: " + Date(System.currentTimeMillis()).toString())
         }
+
+        val btnReadFile: Button = findViewById(R.id.btnReadFile)
+        btnReadFile.setOnClickListener {
+            val data = readFile()
+            tvData.text = data
+        }
+
+        requestNeededPermission()
+    }
+
+    private fun requestNeededPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "I need it for File", Toast.LENGTH_SHORT).show()
+            }
+
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE_WRITE_PERM)
+        } else {
+            Toast.makeText(this, "Already have permission", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_CODE_WRITE_PERM -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "WRITE_EXTERNAL_STORAGE perm granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "WRITE_EXTERNAL_STORAGE perm NOT granted", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun writeFile(data: String) {
+        val file = Environment.getExternalStorageDirectory().toString() + "/test.txt"
+
+        Toast.makeText(this, file, Toast.LENGTH_LONG).show()
+
+        var os: FileOutputStream? = null
+        try {
+            os = FileOutputStream(file)
+            os.write(data.toByteArray())
+            os.flush()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try {
+                os?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun readFile(): String {
+        var result = ""
+        val file = Environment.getExternalStorageDirectory().toString() + "/test.txt"
+        var inputStream: FileInputStream? = null
+
+        try {
+            inputStream = FileInputStream(file)
+            val byteStream = ByteArrayOutputStream()
+            var ch: Int
+            while (inputStream.read().also { ch = it } != -1) {
+                byteStream.write(ch)
+            }
+            result = byteStream.toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try {
+                inputStream?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return result
     }
 }
